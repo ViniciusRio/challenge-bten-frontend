2b21d723ffbf1ce9a35dfeaeab92c47372d545ba
ViniciusRio <34320528+ViniciusRio@users.noreply.github.com>
2021-11-11 21:21:35 -0300

    update(change form)
    add router to create account, add Router, Login obj

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/services/account/account.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  login = {
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+
+  constructor(
+    private router: Router,
+    private accountService: AccountService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  async onSubmit() {
+    try {
+      const result = await this.accountService.login(this.login);
+      console.log(`login efetuado', ${result}`);
+
+      this.router.navigate(['']);
+      
+    } catch (error) {
+      console.error(error);
+      
+    }
   }
 
 }
