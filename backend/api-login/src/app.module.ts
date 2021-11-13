0904b67d856beaa76ef965495dee40f4b0c7ad12
ViniciusRio <34320528+ViniciusRio@users.noreply.github.com>
2021-11-13 17:56:24 -0300

    new(required files for app backend)

---
+++ backend/api-login/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
+import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    LoginModule,
+    TasksModule,
+    UserModule,
+    MongooseModule.forRoot('mongodb+srv://db_user:db_pass@cluster0.wgh3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
