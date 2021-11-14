48ec1f8cdda4075da309f15bdc257e07e817cdde
ViniciusRio <34320528+ViniciusRio@users.noreply.github.com>
2021-11-14 16:19:39 -0300

    update(fix typo)

--- backend/api-login/src/app.module.ts
+++ backend/api-login/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoginModule } from './login/login.module';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
@@ -9,7 +8,6 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    LoginModule,
     TasksModule,
     UserModule,
     MongooseModule.forRoot('mongodb+srv://db_user:db_pass@cluster0.wgh3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CreateAccountComponent } from './pages/create-account/create-account.component';
-import { HomeComponent } from './layouts/home/home.component';
 import { AuthenticationComponent } from './layouts/authentication/authentication.component';
 import { AccountService } from './services/account/account.service';
 import { CommonModule } from '@angular/common';
@@ -19,12 +19,12 @@ import { FormsModule } from '@angular/forms';
     LoginComponent,
     DashboardComponent,
     CreateAccountComponent,
-    HomeComponent,
     AuthenticationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     CommonModule,
     FormsModule
